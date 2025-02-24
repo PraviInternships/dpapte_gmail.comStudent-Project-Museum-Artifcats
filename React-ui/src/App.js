ae2f90f6c339ec04b688cfcb727a3175f303afcb
dineshbugya <dinesh@datasciencelab.in>
2025-02-24 10:36:38 -0500

    Added files for Responsive design

--- React-ui/src/App.js
+++ React-ui/src/App.js
@@ -1,11 +1,17 @@
 // App.js
+import styles from './styles.module.css';
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
 import React from 'react';
 import ArtifactPageContainer from './ArtifactPageContainer';
 
 const App = () => {
     return (
-        <div>
-            <ArtifactPageContainer />
+        <div className="max-w-md mx-auto p-4 bg-white border border-gray-200 rounded">
+            <div className={styles.container}>
+                <ArtifactPageContainer />
+            </div>
         </div>
     );
 };
